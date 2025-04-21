0b6b5761e9757d0690c0a314b6424db81c28d8b5
rawaha <rawahaahmed081@gmail.com>
2025-04-21 17:05:06 +0500

    'commit'

---
+++ project4/01_expression/roll dice/main.py
@@ -0,0 +1,9 @@
+import random
+
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+total = dice1 + dice2
+
+print(f"First die: {dice1}")
+print(f"Second die: {dice2}")
+print(f"Total: {total}")
